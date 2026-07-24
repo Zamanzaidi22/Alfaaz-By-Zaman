3b5c71d64aafbea1c3dc9e740aacfba5abccaf3c
Zamanzaidi22 <zamanzaidi22oct@gmail.com>
2026-07-24 23:41:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,33 @@ searchInput.addEventListener("keyup", function () {
         }
     });
 });
+function showCategory(category){
+
+const shayari = {
+love:
+`❤️ Mohabbat bhi ajeeb si cheez hai,
+Jitni sachchi ho utna dard deti hai.`,
+
+sad:
+`💔 Kuch log sirf yaad bankar reh jaate hain,
+Aur zindagi bhar rulaya karte hain.`,
+
+bewafa:
+`🥀 Wafa ki umeed unse thi,
+Jo bewafa nikle.`,
+
+islamic:
+`🌙 Allah par bharosa rakho,
+Har mushkil ke baad aasani hai.`,
+
+dosti:
+`🤝 Dost woh jo mushkil waqt me saath de.`,
+
+"2line":
+`✨ Alfaaz kam hain,
+Ehsaas bahut gehre hain.`
+};
+
+document.querySelector(".shayari p").innerHTML =
+shayari[category];
+}
