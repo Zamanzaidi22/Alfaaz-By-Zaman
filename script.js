719f47d4db2ad925cfdc3ada396477c6da6424fa
Zamanzaidi22 <zamanzaidi22oct@gmail.com>
2026-07-24 23:37:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Alfaaz By Zaman Loaded");
+const searchInput = document.querySelector("input");
+const cards = document.querySelectorAll(".categories div");
+
+searchInput.addEventListener("keyup", function () {
+    let value = searchInput.value.toLowerCase();
+
+    cards.forEach(card => {
+        if (card.innerText.toLowerCase().includes(value)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+});
