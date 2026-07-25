cf19cd752e79a8f35528f4d586276b48a8e624f0
Zamanzaidi22 <zamanzaidi22oct@gmail.com>
2026-07-25 14:00:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -70,3 +70,14 @@ navigator.clipboard.writeText(text);
 
 alert("✅ Shayari Copied!");
 }
+function shareWhatsApp(){
+
+const text =
+document.getElementById("shayari-text").innerText;
+
+const url =
+"https://wa.me/?text=" + encodeURIComponent(text);
+
+window.open(url, "_blank");
+
+}
