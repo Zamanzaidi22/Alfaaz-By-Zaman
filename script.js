797760cb065c7d8b0319c86ad849986eeda048eb
Zamanzaidi22 <zamanzaidi22oct@gmail.com>
2026-07-31 00:25:19 +0300

    script.js cons category update

--- script.js
+++ script.js
@@ -1122,8 +1122,7 @@ if(window.location.pathname.includes("category.html")){
     const params = new URLSearchParams(window.location.search);
 
     const cat = params.get("cat");
-    alert(cat);
-console.log(cat);
+    alert("Category = " + cat);
 
     const names = {
 
