5ca3c3b81cea3f1d238e4f366966f1fdaf8c3a40
Zamanzaidi22 <zamanzaidi22oct@gmail.com>
2026-07-31 00:08:38 +0300

    script.js new cons search input add

--- script.js
+++ script.js
@@ -55,17 +55,29 @@ console.log("Alfaaz By Zaman Loaded");
 const searchInput = document.querySelector("input");
 const cards = document.querySelectorAll(".categories div");
 
-searchInput.addEventListener("keyup", function () {
-    let value = searchInput.value.toLowerCase();
+if (searchInput) {
 
-    cards.forEach(card => {
-        if (card.innerText.toLowerCase().includes(value)) {
-            card.style.display = "block";
-        } else {
-            card.style.display = "none";
-        }
+    searchInput.addEventListener("keyup", function () {
+
+        let value = searchInput.value.toLowerCase();
+
+        cards.forEach(card => {
+
+            if (card.innerText.toLowerCase().includes(value)) {
+
+                card.style.display = "block";
+
+            } else {
+
+                card.style.display = "none";
+
+            }
+
+        });
+
     });
-});
+
+}
 const dailyQuotes = [
 
 "✨ Har din ek nayi umeed lekar aata hai.",
