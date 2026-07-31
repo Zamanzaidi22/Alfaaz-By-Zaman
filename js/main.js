3416fb0ae1ae2b7a01b1f1cc591be3d80f93f19f
Zamanzaidi22 <zamanzaidi22oct@gmail.com>
2026-07-31 16:57:31 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// ==========================================
+8// ==========================================
 // Alfaaz By Zaman
 // Main File
 // ==========================================
@@ -12,3 +12,26 @@ function initWebsite(){
 }
 
 document.addEventListener("DOMContentLoaded", initWebsite);
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Website Loaded");
+
+    setTimeout(function () {
+
+        const loader = document.getElementById("loader");
+
+        if(loader){
+
+            loader.style.opacity = "0";
+
+            setTimeout(function () {
+
+                loader.style.display = "none";
+
+            },800);
+
+        }
+
+    },1500);
+
+});
