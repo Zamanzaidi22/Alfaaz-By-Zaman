647a764aa9ce553e77cdf9f57385c5e11e00b143
Zamanzaidi22 <zamanzaidi22oct@gmail.com>
2026-07-31 17:26:55 +0300

    Create category.js

---
+++ js/category.js
@@ -0,0 +1,46 @@
+// ==========================================
+// Alfaaz By Zaman
+// Category System
+// ==========================================
+
+let currentCategory = "";
+let currentIndex = 0;
+
+function openCategory(category){
+
+    currentCategory = category;
+    currentIndex = 0;
+
+    showCurrentShayari();
+
+}
+
+function showCurrentShayari(){
+
+    if(!SHAYARI_DB[currentCategory]) return;
+
+    document.getElementById("category-name").innerText =
+        currentCategory.toUpperCase();
+
+    document.getElementById("shayari-text").innerText =
+        SHAYARI_DB[currentCategory][currentIndex];
+
+}
+
+function randomShayari(){
+
+    if(!currentCategory){
+
+        alert("Pehle koi category select karein.");
+
+        return;
+
+    }
+
+    currentIndex = Math.floor(
+        Math.random() * SHAYARI_DB[currentCategory].length
+    );
+
+    showCurrentShayari();
+
+}
