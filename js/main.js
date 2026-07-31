b013ef8e96f79bbe8d9b7be53d8f637c747f8450
Zamanzaidi22 <zamanzaidi22oct@gmail.com>
2026-07-31 15:53:50 +0300

    main.js add code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+// ==========================================
+// Alfaaz By Zaman
+// Main File
+// ==========================================
 
+console.log("🌙 Alfaaz By Zaman V2 Loaded");
+
+function initWebsite(){
+
+    console.log("Website Initialized");
+
+}
+
+document.addEventListener("DOMContentLoaded", initWebsite);
