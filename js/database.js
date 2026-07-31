828ddd4cd9d88c92f77248a2116e09ce9389219d
Zamanzaidi22 <zamanzaidi22oct@gmail.com>
2026-07-31 15:52:55 +0300

    database.js add code

--- js/database.js
+++ js/database.js
@@ -1,1 +1,25 @@
+// ==========================================
+// Alfaaz By Zaman
+// Database File
+// Version 2.0
+// ==========================================
 
+// Complete Shayari Database
+
+const shayari = {
+
+    love: [],
+
+    sad: [],
+
+    bewafa: [],
+
+    islamic: [],
+
+    dosti: [],
+
+    "2line": [],
+
+    zamanDiary: []
+
+};
