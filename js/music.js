953a4a3486aa32586e8cd9695f7ea3919fbf616d
Zamanzaidi22 <zamanzaidi22oct@gmail.com>
2026-08-01 20:02:57 +0300

    Update music.js

--- js/music.js
+++ js/music.js
@@ -1,1 +1,27 @@
+// ==========================================
+// Alfaaz By Zaman
+// Music System
+// ==========================================
 
+let bgMusic = new Audio("assets/music.mp3");
+
+bgMusic.loop = true;
+bgMusic.volume = 0.3;
+
+function toggleMusic(){
+
+    if(bgMusic.paused){
+
+        bgMusic.play();
+
+        showToast("🎵 Music ON");
+
+    }else{
+
+        bgMusic.pause();
+
+        showToast("🔇 Music OFF");
+
+    }
+
+}
