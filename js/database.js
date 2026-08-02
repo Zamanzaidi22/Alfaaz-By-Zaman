603da271b85232c96f1e612e70049ff2c0d0ad12
Zamanzaidi22 <zamanzaidi22oct@gmail.com>
2026-08-02 10:04:15 +0300

    database.js 20 love shairy add

--- js/database.js
+++ js/database.js
@@ -6,7 +6,37 @@
 
 const SHAYARI_DB = {
 
-love: [
+love:[
+
+`Mohabbat lafzon se nahi,
+ehsaason se pehchani jaati hai.`,
+
+`Teri muskurahat meri
+duniya ki sabse haseen jagah hai.`,
+
+`Dil ko sukoon sirf
+tera naam lene se milta hai.`,
+
+`Tum meri duaon ka
+sabse khoobsurat jawab ho.`,
+
+`Mohabbat me jeetna zaroori nahi,
+saath nibhana zaroori hota hai.`,
+
+`Teri yaadon ka safar
+aaj bhi meri rooh me zinda hai.`,
+
+`Har subah tera naam
+meri muskurahat ki wajah ban jata hai.`,
+
+`Dil sirf dhadakta nahi,
+tera intezar bhi karta hai.`,
+
+`Mohabbat me waqt nahi dekha jata,
+sirf dil dekha jata hai.`,
+
+`Tum paas ho ya na ho,
+meri duaon me hamesha rahoge.`,
 
 `Mohabbat lafzon se nahi,
 ehsaason se pehchani jaati hai.`,
@@ -16,6 +46,36 @@ duniya ki sabse haseen jagah hai.`,
 
 `Dil ko sukoon sirf
 tera naam lene se milta hai.`
+
+`Tumhari ek muskurahat,
+mere din ko roshan kar deti hai.`,
+
+`Mohabbat wahi hoti hai,
+jahan dil ko sukoon milta hai.`,
+
+`Tere bina bhi jee rahe hain,
+magar zindagi adhuri si lagti hai.`,
+
+`Tum meri kahani ka
+sabse khoobsurat hissa ho.`,
+
+`Dil ki har dhadkan
+tera hi naam leti hai.`,
+
+`Ishq me faasle mayne nahi rakhte,
+ehsaas hamesha paas rehte hain.`,
+
+`Teri khushi me hi
+meri duniya basi hui hai.`,
+
+`Mohabbat ki sabse badi khoobsurti,
+ek dusre ka saath hota hai.`,
+
+`Tum meri zindagi ka
+wo khwab ho jo kabhi tootna nahi chahiye.`,
+
+`Har baar tujhe soch kar
+dil dua dene lagta hai.`,
 
 ],
 
