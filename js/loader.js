7b026f357eb5dff541896b7593bc4f859a0b1cb3
Zamanzaidi22 <zamanzaidi22oct@gmail.com>
2026-08-05 18:50:36 +0300

    Create loader.js

---
+++ js/loader.js
@@ -0,0 +1,16 @@
+// ==========================================
+// Alfaaz By Zaman
+// Premium Loader
+// ==========================================
+
+window.addEventListener("load", function () {
+
+    const loader = document.getElementById("loader");
+
+    setTimeout(function () {
+
+        loader.classList.add("hide");
+
+    }, 2500);
+
+});
