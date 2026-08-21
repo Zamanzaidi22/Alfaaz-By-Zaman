98fedf74d8a10032f14472440bceccca97d246e2
Zamanzaidi22 <zamanzaidi22oct@gmail.com>
2026-08-21 22:09:46 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,149 @@
+// ==========================================
+// Alfaaz By Zaman
+// Service Worker
+// Basic Offline Cache
+// ==========================================
+
+const CACHE_NAME =
+    "alfaaz-by-zaman-v1";
+
+
+const FILES_TO_CACHE = [
+
+    "./",
+    "./index.html",
+    "./style.css",
+
+    "./manifest.json",
+
+    "./assets/logo.png",
+    "./assets/favicon.png",
+    "./assets/icon-192.png",
+    "./assets/icon-512.png",
+
+    "./js/database.js",
+    "./js/category.js",
+    "./js/search.js",
+    "./js/favorite.js",
+    "./js/download.js",
+    "./js/theme.js",
+    "./js/music.js",
+    "./js/main.js",
+    "./js/counter.js",
+    "./js/loader.js"
+
+];
+
+
+// ==========================================
+// Install
+// ==========================================
+
+self.addEventListener(
+    "install",
+    function(event){
+
+        event.waitUntil(
+
+            caches
+                .open(CACHE_NAME)
+                .then(function(cache){
+
+                    return cache.addAll(
+                        FILES_TO_CACHE
+                    );
+
+                })
+
+        );
+
+        self.skipWaiting();
+
+    }
+);
+
+
+// ==========================================
+// Activate
+// ==========================================
+
+self.addEventListener(
+    "activate",
+    function(event){
+
+        event.waitUntil(
+
+            caches
+                .keys()
+                .then(function(cacheNames){
+
+                    return Promise.all(
+
+                        cacheNames.map(
+                            function(cacheName){
+
+                                if(
+                                    cacheName !==
+                                    CACHE_NAME
+                                ){
+
+                                    return caches.delete(
+                                        cacheName
+                                    );
+
+                                }
+
+                            }
+                        )
+
+                    );
+
+                })
+
+        );
+
+        self.clients.claim();
+
+    }
+);
+
+
+// ==========================================
+// Fetch
+// ==========================================
+
+self.addEventListener(
+    "fetch",
+    function(event){
+
+        if(
+            event.request.method !==
+            "GET"
+        ){
+            return;
+        }
+
+
+        event.respondWith(
+
+            caches
+                .match(event.request)
+                .then(function(cachedResponse){
+
+                    if(cachedResponse){
+
+                        return cachedResponse;
+
+                    }
+
+
+                    return fetch(
+                        event.request
+                    );
+
+                })
+
+        );
+
+    }
+);
